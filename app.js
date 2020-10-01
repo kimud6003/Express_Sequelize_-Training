ac3811e2595545d0e25370a0f83e7e43787e2f77
KUD <kimud6003@gmail.com>
2020-10-01 17:54:04 +0900

    helmet 업데이트

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var helmet = require('helmet')
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -26,6 +28,8 @@ models.sequelize.sync()
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(helmet())
+app.disable('x-powered-by')
 
 app.use(logger('dev'));
 app.use(express.json());
